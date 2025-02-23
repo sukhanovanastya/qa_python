858166377d63e660fc0fca3b126176e16f008200
Anastasia Sukhanova <s.suhanova.nastya@gmail.com>
2025-02-23 19:25:06 +0300

    Sprint_2

--- tests.py
+++ tests.py
@@ -2,7 +2,6 @@ from main import BooksCollector
 
 class TestBooksCollector:
 
-    # Тест для добавления двух книг
     def test_add_new_book_add_two_books(self):
         collector = BooksCollector()
 
@@ -11,7 +10,6 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 2
 
-    # Тест для проверки добавления книги с некорректным названием (пустая строка)
     def test_add_new_book_empty_name(self):
         collector = BooksCollector()
 
@@ -19,7 +17,6 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 0
 
-    # Тест для проверки добавления книги с длинным названием (более 40 символов)
     def test_add_new_book_long_name(self):
         collector = BooksCollector()
 
@@ -28,7 +25,6 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 0
 
-    # Тест для установки жанра книги
     def test_set_book_genre(self):
         collector = BooksCollector()
 
@@ -37,7 +33,6 @@ class TestBooksCollector:
 
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
 
-    # Тест для установки жанра книги с несуществующим жанром
     def test_set_book_genre_invalid_genre(self):
         collector = BooksCollector()
 
@@ -46,7 +41,6 @@ class TestBooksCollector:
 
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == ''
 
-    # Тест для получения книг с определенным жанром
     def test_get_books_with_specific_genre(self):
         collector = BooksCollector()
 
@@ -57,7 +51,6 @@ class TestBooksCollector:
 
         assert collector.get_books_with_specific_genre('Фантастика') == ['Книга 1']
 
-    # Тест для получения книг, подходящих для детей
     def test_get_books_for_children(self):
         collector = BooksCollector()
 
@@ -68,7 +61,6 @@ class TestBooksCollector:
 
         assert collector.get_books_for_children() == ['Книга 1']
 
-    # Тест для добавления книги в избранное
     def test_add_book_in_favorites(self):
         collector = BooksCollector()
 
@@ -77,7 +69,6 @@ class TestBooksCollector:
 
         assert 'Гордость и предубеждение и зомби' in collector.get_list_of_favorites_books()
 
-    # Тест для удаления книги из избранного
     def test_delete_book_from_favorites(self):
         collector = BooksCollector()
 
@@ -86,8 +77,6 @@ class TestBooksCollector:
         collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
 
         assert 'Гордость и предубеждение и зомби' not in collector.get_list_of_favorites_books()
-
-    # Тест для получения списка избранных книг
 
     def test_get_list_of_favorites_books(self):
         collector = BooksCollector()
